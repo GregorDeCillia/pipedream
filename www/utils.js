9f5f2a3b8ccac881ba5b336f737bfc6f3a2520b2
Gregor de Cillia <de.cillia.gregor@gmail.com>
2021-11-08 13:42:45 +0100

    add CRAN link

--- www/utils.js
+++ www/utils.js
@@ -35,7 +35,8 @@ let linkmap = {
   'json API request': 'https://docs.wingarc.com.au/superstar/9.10/open-data-api/use-superweb2-to-build-api-queries',
   'shiny fileInput': 'https://shiny.rstudio.com/reference/shiny/1.6.0/fileInput.html',
   'binary': 'https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/serialize',
-  'httr': 'https://httr.r-lib.org/'
+  'httr': 'https://httr.r-lib.org/',
+  'CRAN': 'https://cran.r-project.org/',
 }
 
 function setLinks() {
