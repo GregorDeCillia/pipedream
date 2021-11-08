caba6c0e97c675566344c0139d080748256704cc
Gregor de Cillia <de.cillia.gregor@gmail.com>
2021-11-08 13:37:12 +0100

    update component table

--- tables/component_database.js
+++ tables/component_database.js
@@ -1,8 +1,8 @@
 const db_data = [
-  ["001", "2021-09-19/11:43", "decill", "Cancer Types", "OGD_krebs_ext_KREBS_1", "Bar", "273 x 4"],
-  ["002", "2021-09-19/11:43", "meindl", "Household stuctures", "OGD_f1741_HH_Proj_1", "Timeseries", "NULL"],
-  ["003", "2021-09-19/11:43", "decill", "Economic trends", "OGD_konjunkturmonitor...", "NULL", "15 x 5"],
-  ["004", "2021-09-19/11:43", "kowa", "Correlations", "statcube", "Scatter", "92 x 9"]
+  ["001", "2021-09-19/11:43", "GregorDeCillia", "Cancer Types by Year", "OGD_krebs_ext_KREBS_1", "NULL", "175 x 3"],
+  ["002", "2021-09-19/14:17", "bernhard-da", "Household Forecast", "OGD_f1741_HH_Proj_1", "Timeseries", "NULL"],
+  ["003", "2021-09-20/06:34", "GregorDeCillia", "Structure of Earnings", "OGD_veste301_Veste301_1", "Barplot", "122 x 7"],
+  ["004", "2021-09-21/13:29", "alexkowa", "Tourism: Correlations", "detouextregsai", "Scatterplot", "92 x 9"]
 ]
 
 $(document).ready(function() {
@@ -12,10 +12,26 @@ $(document).ready(function() {
     dom: 't',
     columns: [
       { title: "PK" },
-      { title: "Created at"},
-      { title: "Creator"},
+      { title: "Created at" },
+      { title: "Creator",
+        render: function(data, type) {
+          if (type === 'display') {
+            return `<a href='https://www.github.com/${data}' target='_blank'>${data}</a>`
+          }
+          return data;
+        }},
       { title: "Custom Label"},
-      { title: "Data Source"},
+      { title: "Data Source",
+        render: function(data, type) {
+          if (type == 'display') {
+            let prefix = data.substring(0, 3) === 'OGD' ?
+              'https://data.statistik.gv.at/web/meta.jsp?dataset=' :
+              'https://statcube.at/statcube/openinfopage?id='
+            return `<a href='${prefix + data}' target='_blank'>${data}</a>`
+          }
+          return data;
+        }
+      },
       { title: "Graph", 
         render: function(data, type) {
           if (type === 'display' & data == 'NULL') {
